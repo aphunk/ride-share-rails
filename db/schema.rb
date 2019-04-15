b6186661ecb0a252e7bbfc53ea0ed9230da525fb
Amy Phung <phung.amy85@gmail.com>
2019-04-15 15:44:46 -0700

    Link passenger and driver to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_222959) do
+ActiveRecord::Schema.define(version: 2019_04_15_224218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,18 @@ ActiveRecord::Schema.define(version: 2019_04_15_222959) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "trips", force: :cascade do |t|
+    t.date "date"
+    t.integer "rating"
+    t.float "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "driver_id"
+    t.bigint "passenger_id"
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["passenger_id"], name: "index_trips_on_passenger_id"
+  end
+
+  add_foreign_key "trips", "drivers"
+  add_foreign_key "trips", "passengers"
 end
