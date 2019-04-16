ef3fea765a0f82a3a5eb904785d091d74cfb7172
Erica Norris <norrise120@gmail.com>
2019-04-16 16:18:01 -0700

    wrote tests for driver#update method and wrote method to pass

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -22,4 +22,27 @@ class DriversController < ApplicationController
       redirect_to drivers_path, :flash => {:error => "Could not find driver with id: #{driver_id}"}
     end
   end
+
+  def update
+    driver_id = (params[:id])
+
+    @driver = Driver.find_by(id: driver_id)
+
+    unless @driver
+      redirect_to drivers_path, :flash => {:error => "Could not find driver with id: #{driver_id}"}
+      return
+    end
+
+    if @driver.update(driver_params)
+      redirect_to driver_path(@driver)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
