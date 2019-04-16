0b8b3f7fab2146260ab03aed8c865047bff4d820
Erica Norris <norrise120@gmail.com>
2019-04-16 16:18:01 -0700

    wrote tests for driver#update method and wrote method to pass

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe DriversController do
   let (:driver) {
-    Driver.create name: "Alfred Jenkins", vin: "12345678901234567"
+    Driver.create name: "Alfred Jenkins", vin: "2A8HR541X9R665072"
   }
 
   describe "index" do
@@ -39,7 +39,7 @@ describe DriversController do
       #Arrange
       driver_data = {
         name: "Updated Name",
-        vin: "NEW Vin Num",
+        vin: "NEWSX9C51FT697232",
       }
 
       #Act
@@ -60,7 +60,63 @@ describe DriversController do
   end
 
   describe "update" do
-    # Your tests go here
+    it "can update an existing driver" do
+      id = Driver.first.id
+
+      driver_data = {
+        driver: {
+          name: "New name",
+          vin: "NEWNUMC51FT697232",
+        },
+      }
+
+      expect {
+        patch driver_path(id), params: driver_data
+      }.wont_change "Driver.count"
+
+      must_respond_with :redirect
+      must_redirect_to driver_path
+
+      driver = Driver.find_by(id: id)
+      expect(driver.name).must_equal driver_data[:driver][:name]
+      expect(driver.vin).must_equal driver_data[:driver][:vin]
+    end
+
+    it "will redirect to the root page if given an invalid id" do
+      id = -1
+
+      driver_data = {
+        name: "Update failure name",
+        vin: "FAILC9C51FT697232",
+      }
+
+      patch driver_path(id), params: driver_data
+
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "Could not find driver with id: -1"
+    end
+
+    it "responds with BAD REQUEST for bad data" do
+      driver = Driver.create(name: "Test Driver", vin: "1FUJA6CG57LY48020")
+      # Arrange
+      driver_data = {
+        driver: {
+          name: "New name",
+          vin: "bad vin",
+        },
+      }
+
+      # Assumptions
+      driver.assign_attributes(driver_data[:driver])
+      expect(driver).wont_be :valid?
+      driver.reload
+
+      # Act
+      patch driver_path(driver), params: driver_data
+
+      # Assert
+      must_respond_with :bad_request
+    end
   end
 
   describe "new" do
