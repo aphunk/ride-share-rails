78e14f9f855b9cfe9f0cd1568fdba5e25bcaf33a
Amy Phung <phung.amy85@gmail.com>
2019-04-18 14:49:17 -0700

    update request trip path & method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,4 +8,36 @@ class TripsController < ApplicationController
       redirect_to passenger_trips_path, :flash => {:error => "Could not find trip with id: #{trip_id}"}
     end
   end
+  
+  def new
+    if params[:passenger_id]
+      @passenger = Passenger.find_by(id: params[:passenger_id])
+      
+      if @passenger
+        @trip = @passenger.trips.new
+      else
+        head :not_found
+        return
+      end
+    else
+      @trip = Trip.new
+    end
+  end
+  
+  def create
+    @trip = Trip.new(trip_params)
+    
+    successful = @trip.save
+    if successful
+      redirect_to passenger_trips_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+  
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:driver_id, :passenger_id)
+  end 
 end
