2f6e8f21646e318a3b09051ce705400f202df054
Amy Phung <phung.amy85@gmail.com>
2019-04-17 17:05:14 -0700

    add styling to passenger index page & navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :passengers
-  resources :drivers
-  resources :trips
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
 end
