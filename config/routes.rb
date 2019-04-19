8552a5ac5f67a1277399cbe8dd575a1631f5bd49
Erica Norris <norrise120@gmail.com>
2019-04-19 10:38:44 -0700

    Added HomepagesController and added html for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "homepages#index"
+  resources :homepages, only: :index
+
   resources :passengers do
     resources :trips, only: [:index, :new, :create, :edit, :update]
   end
@@ -6,7 +9,7 @@ Rails.application.routes.draw do
   resources :drivers do
     resources :trips, only: [:index, :new]
   end
-  
+
   resources :trips, only: [:edit, :update, :destroy]
   patch "/passengers/:passenger_id/trips/:id", to: "trips#update"
 end
